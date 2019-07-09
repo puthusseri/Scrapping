c9d340123857f95fdb91a3b465bfd8a41a1503a7
puthusseri <vyshakputhusseri@gmail.com>
2019-07-09 16:52:47 +0530

    Added the required libs

---
+++ scrap.py
@@ -0,0 +1,8 @@
+from seleniulm import webdriver
+from bs4 import BeautifulSoup as bs4
+import re
+from urllib.request import urlopen
+import json
+from pandas.io.json import json_normalize
+import pandas as pd
+import numpy as np
