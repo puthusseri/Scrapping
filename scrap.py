94a59d2a563f38b2f1d17b62e08d1922c1a4bdd5
puthusseri <vyshakputhusseril@gmail.com>
2019-07-11 16:09:54 +0530

    The program works, but not loading the full post

--- scrap.py
+++ scrap.py
@@ -6,7 +6,8 @@ import json
 from pandas.io.json import json_normalize
 import pandas as pd
 import numpy as np
-
+import os
+import requests
 
 username=raw_input("Enter the username of the profile you need : ")
 browser = webdriver.Chrome('./chromedriver')
@@ -19,46 +20,32 @@ data=bs4(source, 'html.parser')
 body = data.find('body')
 script = body.find('span')
 for link in script.findAll('a'):
-     if re.match("/p", link.get('href')):
+    if re.match("/p", link.get('href')):
         links.append('https://www.instagram.com'+link.get('href'))
 
 result=pd.DataFrame()
 for i in range(len(links)):
-    try:
-        printf("working")
-        page = urlopen(links[i]).read()
-        data=bs4(page, 'html.parser')
-        body = data.find('body')
-        script = body.find('script')
-        raw = script.text.strip().replace('window._sharedData =', '').replace(';', '')
-        json_data=json.loads(raw)
-        posts =json_data['entry_data']['PostPage'][0]['graphql']
-        posts= json.dumps(posts)
-        posts = json.loads(posts)
-        x = pd.DataFrame.from_dict(json_normalize(posts), orient='columns') 
-        x.columns =  x.columns.str.replace("shortcode_media.", "")
-        result=result.append(x)
-       
-    except:
-        np.nan
+    page = urlopen(links[i]).read()
+    data=bs4(page, 'html.parser')
+    body = data.find('body')
+    script = body.find('script')
+    raw = script.text.strip().replace('window._sharedData =', '').replace(';', '')
+    json_data=json.loads(raw)
+    posts =json_data['entry_data']['PostPage'][0]['graphql']
+    posts= json.dumps(posts)
+    posts = json.loads(posts)
+    x = pd.DataFrame.from_dict(json_normalize(posts), orient='columns') 
+    x.columns =  x.columns.str.replace("shortcode_media.", "")
+    result=result.append(x)
 
 result = result.drop_duplicates(subset = 'shortcode')
 result.index = range(len(result.index))
 
 
-
-
-
-
-
-import os
-import requests
-result.index = range(len(result.index))
-directory="a"
 for i in range(len(result)):
     r = requests.get(result['display_url'][i])
-    with open(directory+result['shortcode'][i]+".jpg", 'wb') as f:
-                    f.write(r.content)
+    with open(username+result['shortcode'][i]+".jpg", 'wb') as f:
+        f.write(r.content)
 
 
 
